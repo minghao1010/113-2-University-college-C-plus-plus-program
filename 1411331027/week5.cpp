9010684111286a221ab3369ca91a8b4f38b7a89b
s1411331027 <153046804+minghao1010@users.noreply.github.com>
2025-03-19 11:05:36 +0800

    Add files via upload

---
+++ 1411331027/week5.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+using namespace std;
+
+// GradeBook class definition
+class GradeBook
+{
+public:
+    // function that displays a welcome message to the GradeBook user
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+    } // end function displayMessage
+    void changeID()
+    {
+        cout << "請輸入ID" << endl;
+        cin >> myID;
+    }
+    void printID()
+    {
+        cout << "我的ID:" << myID << endl;
+    }
+private:
+    int myID = 0;
+}; // end class GradeBook  
+
+// function main begins program execution
+int main()
+{
+    GradeBook myGradeBook,book1,book2,book3; // create a GradeBook object named myGradeBook
+    myGradeBook.displayMessage();
+    myGradeBook.printID();
+    myGradeBook.changeID();
+    myGradeBook.printID();
+    
+} // end main
