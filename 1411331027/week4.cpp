b1d6ca9233ea23566b63b9e2a0163445052d0f8d
s1411331027 <153046804+minghao1010@users.noreply.github.com>
2025-03-19 10:13:08 +0800

    Add files via upload

---
+++ 1411331027/week4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int volume(int);               
+double volume(int, int);         
+int volume(int, int, int);       
+
+int main()
+{
+    double sideValue = 10.0;
+
+    int a, b, c;
+    float result;
+
+    
+    cout << "請輸入一個整數" << endl;
+    cin >> a;
+    cout << "立方體的體積是: " << volume(a) << endl;
+    cout << "請輸入兩個整數" << endl;
+    cin >> a >> b;
+    cout << "圓柱的體積是: " << volume(a, b) << endl;
+    cout << "請輸入三個整數" << endl;
+    cin >> a >> b >> c;
+    cout << "長方體的體積是: " << volume(a, b, c) << endl;
+
+    cout << "finished!" << endl;
+}
+
+int volume(int a)
+{
+    return a * a * a;
+}
+
+double volume(int a, int b)
+{
+    return a * a * 3.14159 * b;
+}
+
+int volume(int a, int b, int c)
+{
+    return a * b * c;
+}
