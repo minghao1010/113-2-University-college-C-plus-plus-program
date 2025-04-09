9f2e7b1781f4f5b17dad9120aad3675b19a78ff3
s1411331027 <153046804+minghao1010@users.noreply.github.com>
2025-04-09 11:44:24 +0800

    Add files via upload

---
+++ 1411331027/week8/week8-2/week8-2/week8-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "Date.h"
+using namespace std;
+
+int main() {
+    int y, m, d;
+
+    cout << "請輸入西元年、月、日（例如 2025 4 9）: ";
+    cin >> y >> m >> d;
+
+    try {
+        Date date(y, m, d);         // 建構時自動驗證
+        date.printGregorian();      // 顯示西元
+        date.printROC();            // 顯示民國
+    }
+    catch (invalid_argument& e) {
+        cerr << "錯誤: " << e.what() << endl;
+    }
+
+    return 0;
+}
